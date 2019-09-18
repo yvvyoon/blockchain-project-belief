4ed735d1f27e94d10067658afd9f58a729001a5d
ssorrychoi <zzaesung17@gmail.com>
2019-09-18 18:06:41 +0900

    modify

--- agency_web/src/User.js
+++ agency_web/src/User.js
@@ -5,8 +5,9 @@ function User()  {
     return (
       <div style={styles.div} className="User">
         <h1 style={styles.h1}>User</h1>
-        <input style= {styles.input} name="User_HashCode" type="text" placeholder="HashCode"></input>
-        <input style= {styles.input} name="User_OTP" type="text" placeholder="OTP"></input>
+        <input style= {styles.input} name="User_HashCode" type="tel" placeholder="핸드폰 번호를 입력해주세요"></input>
+        {/* <input style= {styles.input} name="User_HashCode" type="text" placeholder="HashCode"></input>
+        <input style= {styles.input} name="User_OTP" type="text" placeholder="OTP"></input> */}
         <button>Search</button>
       </div>
     );
@@ -17,7 +18,7 @@ const styles = {
     marginTop : '50px',
   },
   input : {
-    width : '70%',
+    width : '50%',
     margin: '25px',
     textAlign: 'center'
   }
