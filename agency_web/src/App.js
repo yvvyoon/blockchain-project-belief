a68a83f98fefd05df8df3b32917ef279d6ad0d5f
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2019-10-09 17:51:12 +0900

    20191009

--- agency_web/src/App.js
+++ agency_web/src/App.js
@@ -1,39 +1,15 @@
 import React from 'react';
-import Agency from './Agency';
-import User from './User';
-import Government from './Government';
+import Attendance from './Attendance';
 
 import './App.css';
 
 class App extends React.Component {
 
-  state = {
-    isUser: true,
-    isAgency: false,
-    isGovernment: false,
-  }
-
-  User = () => {
-    this.setState({ isUser: true, isAgency: false, isGovernment: false })
-  };
-
-  Agency = () => {
-    this.setState({ isUser: false, isAgency: true, isGovernment: false })
-  };
-
-  Government = () => {
-    this.setState({ isUser: false, isAgency: false, isGovernment: true })
-  };
-
   render() {
-    const { isUser, isAgency } = this.state;
     return (
       <div className="App">
         <center>
-        <button onClick={this.User}>User</button>
-        <button onClick={this.Agency}>Agency</button>
-        <button onClick={this.Government}>Government</button>
-        {isUser ? <User /> : isAgency ? <Agency /> : <Government />}
+          <Attendance />
         </center>
       </div>
     );
