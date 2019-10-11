c15c99e7b4e900e9b9a1ce9e0f2f6701a1a9dd03
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2019-10-11 15:41:46 +0900

    20191011

--- mainPageServer/app.js
+++ mainPageServer/app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var sequelize = require('./models').sequelize;
 
 var app = express();
+sequelize.sync();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +20,16 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('secret code'));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'secret code',
+  cookie: {
+    httpOnly: true,
+    secure: false
+  }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- mainPageServer/routes/users.js
+++ mainPageServer/routes/users.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models').User;
+var cors = require('cors');
+router.use(cors());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-  console.log('하이 : ',req.query.result);
+router.get('/', function (req, res, next) {
+  User.findAll({
+    where: {
+      number: req.query.result
+    }
+  })
+    .then((users) => {
+      req.session.result = JSON.stringify(users);
+      console.log('req.session.result : ', req.session.result);
+      let result = req.session.result;
+      let result2 = JSON.parse(result)[0];
+       req.session.name = result2.name,
+       req.session.time = result2.time,
+       req.session.type = result2.number
+       console.log('req.session.name : ', req.session.name);
+       res.json({
+        name: req.session.name,
+        time: req.session.time,
+        type: req.session.type
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    })
 });
 
 module.exports = router;
